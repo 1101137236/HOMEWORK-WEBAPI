73acc44a24caf0a2fffff452986859bb2a92967f
1101137236 <w2497imt@yahoo.com.tw>
2015-05-03 20:31:39 +0800

    name

---
+++ MvcApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,49 @@
+﻿using KuasCore.Models;
+using KuasCore.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace MvcApplication1.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEmployeeService EmployeeService { get; set; }
+
+        [HttpGet]
+        [ActionName("byName")]
+        public Employee GetEmployeeByName(string name)
+
+        {
+            var employee = EmployeeService.GetEmployeeByName(name);
+
+            if (employee == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return employee;
+        }
+
+
+
+
+        //[HttpGet]
+        //[ActionName("byName")]
+        //public Employee GetEmployeeByName(string name)
+        //{
+        //    var employee = EmployeeService.GetEmployeeByName(name);
+
+        //    if (employee == null)
+        //    {
+        //        throw new HttpResponseException(HttpStatusCode.NotFound);
+        //    }
+
+        //    return employee;
+        //}
+    }
+}
